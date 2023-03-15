7869c3737f893998c7a14d96134c222d798e7a4f
Yrysbu <yrysbu.nasirdinova.kk@gmail.com>
2023-03-15 10:22:23 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// id=4
+// fetch(`https://swapi.dev/api/people/${id}`)
+//   .then((response) => {
+//     return response.json()
+//   })
+//     .then((data) => alert(`Name: ${data.name} age: ${data.birth_year} `))
+
+
+// id = 4
+// fetch(`https://swapi.dev/api/vehicles/ ${id}`)
+//     .then((response) => {
+//     return response.json()
+//     })
+// .then((data)  => alert (`Name : ${data.name} crew:${data.crew}` ))
+
+// id = 9
+// fetch(`https://swapi.dev/api/starships/${id}`)
+//     .then((response) => {
+//     return response.json()
+//     })
+// .then((data) => alert (`Name: ${data.name} model: ${data.model}`))
+
+
+
+
